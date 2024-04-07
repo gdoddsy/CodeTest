e16e6d803d926de6975d8df23c043bf6e34ef242
Greg.Dodd <Greg.Dodd@redox.com>
2024-04-08 07:31:41 +1000

    Update to API to fix CORS issues

--- CodeTestWebApp/Program.cs
+++ CodeTestWebApp/Program.cs
@@ -1,8 +1,20 @@
+using Microsoft.AspNetCore.Mvc.Formatters;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options => {options.OutputFormatters.RemoveType<StringOutputFormatter>();})
+	.AddJsonOptions(options =>
+	{
+		options.JsonSerializerOptions.IncludeFields = true;
+		options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+		options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+		options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+	});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -16,6 +28,11 @@ builder.Services.ConfigureSwaggerGen(setup =>
 });
 
 var app = builder.Build();
+
+app.UseCors(cors => cors
+	.AllowAnyOrigin()
+	.AllowAnyMethod()
+	.AllowAnyHeader());
 
 app.UseSwagger();
 app.UseSwaggerUI();
